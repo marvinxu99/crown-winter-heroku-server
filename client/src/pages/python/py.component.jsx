47578543fac749b9fa34dbd6959968f34cc30b81
Marvin <44047272+marvinxu99@users.noreply.github.com>
2019-12-08 00:37:57 -0800

    open in a separate page

--- client/src/pages/python/py.component.jsx
+++ client/src/pages/python/py.component.jsx
@@ -9,8 +9,9 @@ const PyPage = () => (
   <div class='py-page-container'>
     <h2>Python3</h2>
     <ul>
-      <li><a href="https://docs.python.org/3/">Python3</a></li>
-      <li><a href="https://www.makeuseof.com/tag/python-instance-static-class-methods/">Python methods</a></li>
+      <li><a href="https://docs.python.org/3/" target='_blank' rel='noopener noreferrer'>Python3</a></li>
+      <li><a href="https://www.makeuseof.com/tag/python-instance-static-class-methods/"
+        target='_blank' rel='noopener noreferrer'>Python methods</a></li>
       <li>...more on the way</li>
     </ul>
 
